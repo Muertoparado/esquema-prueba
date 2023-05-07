9f6ab2c2f0181e9d904605a8f52a6a68b5d502a2
Muertoparado <nicolas.casanova20@gmail.com>
2023-05-07 18:18:26 -0500

    feat(rama usuarios): :rocket: beer

---
+++ api/render.js
@@ -0,0 +1,60 @@
+const results= document.querySelector("#renderproducto");
+let puerto = 4005;
+
+
+/* const renderProductos = async ()=>{
+    const response= await fetch(`http://localhost:${puerto}/productos`)
+    const data = await response.json();
+    console.log(data);
+    return data;
+} */
+
+(async () =>{
+
+    console.log("inicio");
+    const response= await fetch(`http://localhost:4005/productos`)
+    const data = await response.json();
+      console.log(data);
+
+      for (let i = 0; i < data.length; i++) {
+        const card = document.createElement('div');
+        card.classList.add('card');
+        card.innerHTML = `
+    
+    <div class="bg-warning text-center ">
+        <input type="checkbox" id="custom-checkbox" class="custom-checkbox" />
+      <label for="custom-checkbox" class="custom-label">
+          <div class="card">
+            <h1>${data[i].name}</h1>
+            <p>id: ${data[i].id}</p>
+            <h3>costo: $${data[i].costo}</h3>
+          </div>
+       </label>
+    </div>
+       `;
+       results.appendChild(card);
+       };
+     
+      
+})();
+
+
+/* */
+
+   /*  let html = ``;  
+
+      data.productos.forEach(element => {    
+          html += `
+          <div class="bg-warning">
+          <input type="checkbox" id="custom-checkbox" class="custom-checkbox" />
+      <label for="custom-checkbox" class="custom-label">
+          <div class="cards">
+          <h1>${element.nombre}</h1>
+         <h3>costo: $${element.costo}</h3>
+          </div>
+       </label>
+         </div>
+          `
+      });
+      results.appendChild(html);
+      return html;  */
--- app.js
+++ app.js
@@ -1,1 +1,39 @@
 import "./components/div-productos.js"
+import "./components/div-usuarios.js"
+import "./api/render.js"
+
+
+const modal = document.querySelector("#modal");
+const mymodal = document.querySelector("#mymodal");
+
+function openModal() {
+    mymodal.classList.add('show');
+    mymodal.style.display = 'block';
+    backdrop = document.createElement('div');
+    backdrop.className = 'modal-backdrop fade show';
+    document.body.appendChild(backdrop);
+  }
+
+  function closeModal() {
+    mymodal.classList.remove('show');
+    mymodal.style.display = 'none';
+    if (backdrop) {
+      document.body.removeChild(backdrop);
+    }
+  }
+
+  modal.addEventListener("click", openModal);
+  cerrarmodalx.addEventListener("click", closeModal);
+  cerrarmodal.addEventListener("click", closeModal);
+
+/* 
+modal.addEventListener("click",()=>{
+
+    mymodal.classList.add('show');
+    mymodal.style.display = 'block';
+    // Agregar una clase al fondo para que se oscurezca al abrir el modal
+    var backdrop = document.createElement('div');
+    backdrop.className = 'modal-backdrop fade show';
+    document.body.appendChild(backdrop);
+  });
+ */
